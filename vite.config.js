31a6f638841f7e4ac554106ee2e9eaba10795d43
Navya Jede <navyajede2005@gmail.com>
2026-01-12 01:23:10 +0530

    fix vercel blank screen

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/T-homeFinetech/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
